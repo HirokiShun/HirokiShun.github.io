13b1e5f4a648b710a5bd5cb68dbe10cd5f16b7ce
Diego Leos <diego.heriberto@hotmail.com>
2022-09-13 01:28:11 -0500

    faltan vaidaciones

--- js/Login.js
+++ js/Login.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
 const formulario = document.getElementById('Register');
 const inputs = document.querySelectorAll('#Register input');
@@ -8,20 +7,19 @@ const expresiones = {
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	password: /^.{4,12}$/, // 4 a 12 digitos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
 }
 
 const campos = {
 	usuario: false,
 	nombre: false,
 	password: false,
-	correo: false,
-	telefono: false
+	correo: false
 }
 
 const validarFormulario = (e) => {
 	switch (e.target.name) {
 		case "user":
+			console.log("user");
 			validarCampo(expresiones.usuario, e.target, 'user');
 		break;
 		case "Name":
@@ -43,14 +41,14 @@ const validarFormulario = (e) => {
 const validarCampo = (expresion, input, campo) => {
 	if(expresion.test(input.value)){
 		document.getElementById(`group__${campo}`).classList.remove('login-form__group-incorrect');
-		document.getElementById(`group__${campo}`).classList.add('login-form__group-correcto');
+		document.getElementById(`group__${campo}`).classList.add('login-form__group-correct');
 		document.querySelector(`#group__${campo} i`).classList.add('fa-check-circle');
 		document.querySelector(`#group__${campo} i`).classList.remove('fa-times-circle');
 		document.querySelector(`#group__${campo} .login-form__input-error`).classList.remove('login-form__input-error-active');
 		campos[campo] = true;
 	} else {
 		document.getElementById(`group__${campo}`).classList.add('login-form__group-incorrect');
-		document.getElementById(`group__${campo}`).classList.remove('login-form__group-correcto');
+		document.getElementById(`group__${campo}`).classList.remove('login-form__group-correct');
 		document.querySelector(`#group__${campo} i`).classList.add('fa-times-circle');
 		document.querySelector(`#group__${campo} i`).classList.remove('fa-check-circle');
 		document.querySelector(`#group__${campo} .login-form__input-error`).classList.add('login-form__input-error-active');
@@ -64,14 +62,14 @@ const validarPassword2 = () => {
 
 	if(inputPassword1.value !== inputPassword2.value){
 		document.getElementById(`group RepeatPass`).classList.add('login-form__group-incorrect');
-		document.getElementById(`group RepeatPass`).classList.remove('login-form__group-correcto');
+		document.getElementById(`group RepeatPass`).classList.remove('login-form__group-correct');
 		document.querySelector(`#group__RepeatPass i`).classList.add('fa-times-circle');
 		document.querySelector(`#group__RepeatPass i`).classList.remove('fa-check-circle');
 		document.querySelector(`#group__RepeatPass .login-form__input-error`).classList.add('login-form__input-error-active');
 		campos['pass'] = false;
 	} else {
 		document.getElementById(`group__RepeatPass`).classList.remove('login-form__group-incorrect');
-		document.getElementById(`group__RepeatPass`).classList.add('login-form__group-correcto');
+		document.getElementById(`group__RepeatPass`).classList.add('login-form__group-correct');
 		document.querySelector(`#group__RepeatPass i`).classList.remove('fa-times-circle');
 		document.querySelector(`#group__RepeatPass i`).classList.add('fa-check-circle');
 		document.querySelector(`#group__RepeatPass .login-form__input-error`).classList.remove('login-form__input-error-active');
@@ -86,6 +84,19 @@ inputs.forEach((input) => {
 
 formulario.addEventListener('submit', (e) => {
 	e.preventDefault();
+	if(campos.usuario && campos.nombre && campos.password && campos.correo ){
+		formulario.reset();
 
+		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+		setTimeout(() => {
+			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+		}, 5000);
+
+		document.querySelectorAll('.formulario__grupo-correct').forEach((icono) => {
+			icono.classList.remove('formulario__grupo-correct');
+		});
+	} else {
+		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+	}
 })
 });
